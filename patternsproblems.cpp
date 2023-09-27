358629d1364dbbb1cdb74ab99957584ceb32f69d
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-28 00:43:02 +0530

    Solved hard ques of patterns. Mix ques of spaces, numbers, and stars.

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -7,11 +7,18 @@ int main() {
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n-i+1; j++)
+        for(int j = 1; j <= 2*(n-1)-i+1; j++)
         {
-            if(i == 1 || j == 1 || j == n-i+1)
-                cout << "* ";
-            else cout << "  ";
+            cout << "*";
+        }
+        for(int j = 1; j <= 2*i-1; j++)
+        {
+            if(j % 2 == 0) cout << "*";
+            else cout << i;
+        }
+        for(int j = 1; j <= 2*(n-1)-i+1; j++)
+        {
+            cout << "*";
         }
         cout << endl;
     }
