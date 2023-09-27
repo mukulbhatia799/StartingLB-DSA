323b7ba97497eb7d3d4e1cd80d637ebfcf6a85af
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-27 21:42:53 +0530

    solved hollow triangle without base or third side pattern

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -5,27 +5,16 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= n-i; j++)
-        {
-            cout << " ";
-        }
-        for(int j = 1; j <= i; j++)
-        {
-            cout << "* ";
-        }
-        cout << endl;
-    }
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < i; j++)
+        for(int j = i; j < n-1; j++)
         {
             cout << " ";
         }
-        for(int j = 0; j < n-i; j++)
+        for(int j = 0; j <= i; j++)
         {
-            cout << "* ";
+            if(i == j || j == 0) cout << "* ";
+            else cout << "  ";
         }
         cout << endl;
     }
