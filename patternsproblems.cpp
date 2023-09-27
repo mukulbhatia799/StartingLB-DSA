a59f4ad9cb50e0e877fb84dee6f5a01ef4e95b27
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-28 00:35:37 +0530

    solved rotated hollow triangle problem

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -7,10 +7,11 @@ int main() {
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= 2*i-1; j++)
+        for(int j = 1; j <= n-i+1; j++)
         {
-            if(j % 2 == 0) cout << "*";
-            else cout << i;
+            if(i == 1 || j == 1 || j == n-i+1)
+                cout << "* ";
+            else cout << "  ";
         }
         cout << endl;
     }
