ddf1d23b9e7d51a6b0349cb947b6559293cbd18d
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-27 21:36:51 +0530

    solved diamond pattern

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -9,9 +9,21 @@ int main() {
     {
         for(int j = 1; j <= n-i; j++)
         {
-            cout << "  ";
+            cout << " ";
         }
-        for(int j = 1; j <= 2*i-1; j++)
+        for(int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for(int j = 0; j < n-i; j++)
         {
             cout << "* ";
         }
