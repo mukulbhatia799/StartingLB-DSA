3828d08b60504a0d01e5552c8a0cb985d31c622a
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-27 21:10:50 +0530

    solved hollow inverted half pyramid

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -1,20 +1,25 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int main(){
-	int n;
-	cin >> n;
+int main() {
+    int n;
+    cin >> n;
 
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			if(i == 0 || j == 0 || i == n-1 || j == n-1) cout << "* ";
-			else cout << "  ";
-		}
-		cout << endl;
-	}
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i; j < n; j++)
+        {
+            if(i == 0 || j == i || j == n-1)
+            {
+                cout << "* ";
+            }
+            else 
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
 
-
-    return 0; 
+    return 0;
 }
