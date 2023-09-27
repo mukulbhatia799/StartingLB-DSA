81f3d9c9daf9ef38254f1c873411290bd615b74c
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-27 21:51:12 +0530

    solved rotated hollow triangle without base pattern:

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -5,16 +5,18 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i <= n; i++)
     {
-        for(int j = i; j < n-1; j++)
+        for(int j = 1; j < i; j++)
         {
             cout << " ";
         }
-        for(int j = 0; j <= i; j++)
+        for(int j = 1; j <= n-i+1; j++)
         {
-            if(i == j || j == 0) cout << "* ";
-            else cout << "  ";
+            if(j == 1 || j == n-i+1)
+                cout << "* ";
+            else 
+                cout << "  ";
         }
         cout << endl;
     }
