58cc4d624107d1a32b3f753cf236813d2fbc4bbb
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-28 00:31:50 +0530

    solved numbers and stars mix ques

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -5,35 +5,12 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i = 1; i <= n/2; i++)
+    for(int i = 1; i <= n; i++)
     {
-        for(int j = i; j <= n/2; j++)
-        {
-            cout << "* ";
-        }
         for(int j = 1; j <= 2*i-1; j++)
         {
-            cout << "  ";
-        }
-        for(int j = i; j <= n/2; j++)
-        {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-    for(int i = 1; i <= n/2; i++)
-    {
-        for(int j = 1; j <= i; j++)
-        {
-            cout << "* ";
-        }
-        for(int j = 1; j <= 2*((n/2)-i+1)-1; j++)
-        {
-            cout << "  ";
-        }
-        for(int j = 1; j <= i; j++)
-        {
-            cout << "* ";
+            if(j % 2 == 0) cout << "*";
+            else cout << i;
         }
         cout << endl;
     }
