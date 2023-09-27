0d7130cff6d68ef35acb5e0c9dcf25b8d9e81050
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-27 22:08:28 +0530

    solved diamond hollow problem by taking two variables and updating them

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -5,20 +5,39 @@ int main() {
     int n;
     cin >> n;
 
+    // initialize n with even number only.
+    // if odd is taken then n-1 will be printed.
+    int stars = 1, space = n/2;
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j < i; j++)
+        // spaces
+        for(int j = 1; j < space; j++)
         {
-            cout << " ";
+            cout << "  ";
         }
-        for(int j = 1; j <= n-i+1; j++)
+        // stars
+        for(int j = 1; j <= stars; j++)
         {
-            if(j == 1 || j == n-i+1)
+            if(j == 1 || j == stars)
                 cout << "* ";
             else 
                 cout << "  ";
         }
         cout << endl;
+        if(i >= n/2)
+        {
+            if(i == n/2) continue;
+            else 
+            {
+                space++;
+                stars -= 2;
+            }
+        }
+        else
+        {
+            space--;
+            stars += 2;
+        }
     }
 
     return 0;
