03c580a6c88cddf7297beb955d5c039b61827089
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-29 00:46:39 +0530

    solved butterfly pattern

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -4,15 +4,29 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
+    
+    // n should be even.
+    int stars = 1, spaces = n-1;
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n; j++)
-        {
-            if(i == 1 || j == 1 || j == n || i == n || ((i == n/2 || i == n/2+1) && (j == n/2 || j == n/2+1))) cout << "* ";
-            else cout << "  ";
-        }
+        for(int j = 1; j <= stars; j++)
+            cout << "* ";
+        for(int j = 1; j <= spaces; j++)
+            cout << "  ";
+        for(int j = 1; j <= stars; j++)
+            cout << "* ";
         cout << endl;
+        if(i < (n+1)/2)
+        {
+            stars++;
+            spaces-=2;
+        }
+        else if(i == (n+1)/2) continue;
+        else 
+        {
+            stars--;
+            spaces+=2;
+        }
     }
 
     return 0;
