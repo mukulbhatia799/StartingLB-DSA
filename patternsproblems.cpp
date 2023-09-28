7b1e7b8b7eb040a0976819a16acb6aac61514747
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-28 23:17:46 +0530

    solved hollow numbers triangle pattern problem

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -7,18 +7,12 @@ int main() {
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= 2*(n-1)-i+1; j++)
-        {
-            cout << "* ";
-        }
         for(int j = 1; j <= 2*i-1; j++)
         {
-            if(j % 2 == 0) cout << "* ";
-            else cout << i << " ";
-        }
-        for(int j = 1; j <= 2*(n-1)-i+1; j++)
-        {
-            cout << "* ";
+            if(j == 1 || j == 2*i-1 || i == n)
+                cout << j << " ";
+            else 
+                cout << "  ";
         }
         cout << endl;
     }
