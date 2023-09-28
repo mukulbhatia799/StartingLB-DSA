a1943003b3485d780edaa1f3d4ef1aa842651f47
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-28 23:23:08 +0530

    solved reverse alphabets triangle pattern problem

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -7,12 +7,12 @@ int main() {
 
     for(int i = 1; i <= n; i++)
     {
+        char c = 'A';
         for(int j = 1; j <= 2*i-1; j++)
         {
-            if(j == 1 || j == 2*i-1 || i == n)
-                cout << j << " ";
-            else 
-                cout << "  ";
+            cout << c << " ";
+            if(j < (2*i)/2) c++;
+            else c--;
         }
         cout << endl;
     }
