ac9c4cf6533c9990bf0fa1853447279bea621407
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-01 16:03:15 +0530

    convert decimal to binary digits. store the bits in string as starting bits could be 0.

--- basics.cpp
+++ basics.cpp
@@ -1,31 +1,32 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+void convertdecimaltobinary(int n)
+{
+    string ans = "";
+
+    while(n != 0)
+    {
+        ans = to_string(n%2) + ans;
+        n /= 2;
+    }
+    for(int i = 0; i < ans.length(); i++)
+    {
+        cout << ans[i];
+    }
+    cout << endl;
+}
+
 int main(){
+    
+    int n;
+    cin >> n;
 
-    // remember, the calculation is done from right to left .
-
-    int n = 10;
-
-    int ans;
-
-    cout << ++n * n++ << endl;
-    /*
-    first ++n = 11 then * then n++ = 12 so 132 is answer.
-    */
-
-
-
-    // cout << n++ * ++n << endl;
-    n = 10;
-    // ans = n++ * ++n;
-
-    cout << n++ * ++n << endl;
-
-
-    /*
-    first n++ = 11 then * then ++n = 1
-    */
+    convertdecimaltobinary(n);
 
     return 0;
 }
+
+// first make string variable where the bits will be stored.
+// add the bit in front of the string using + operator.
+// and divide n by 2 in each iteration.
