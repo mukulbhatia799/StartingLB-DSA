0e69d293ed22a4a689454df600c181e47dfd9eda
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-01 18:01:06 +0530

    updated: created default constructor when any data value is not assigned to a node.

--- basics.cpp
+++ basics.cpp
@@ -6,14 +6,19 @@ class Node
 public:
     int data;
     Node* next;
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
     Node(int data)
     {
         this->data = data;
         
     }
-    void display(Node* head)
+    void display()
     {
-        Node* temp = head;
+        Node* temp = this;
         while(temp != NULL)
         {
             cout << temp->data << " ";
@@ -34,7 +39,16 @@ int main(){
     third->next = fourth;
     fourth->next = NULL;
 
-    first->display(first);    
+    cout << "New display: ";
+    first->display();  
+
+    Node* fifth = new Node;
+    fourth->next = fifth;
+    fifth->next = NULL;
+
+    cout << "New display: ";
+
+    first->display();
 
     return 0;
 }
