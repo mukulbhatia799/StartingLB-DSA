3e3e4761f5b5f512474d2f6c192faa93aa238ccd
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-01 13:49:40 +0530

    special ques of pre/post increment which are compiler dependent.

--- basics.cpp
+++ basics.cpp
@@ -2,15 +2,30 @@
 using namespace std;
 
 int main(){
+
+    // remember, the calculation is done from right to left .
+
     int n = 10;
 
-    int ans = ++n * n++;
+    int ans;
 
+    cout << ++n * n++ << endl;
     /*
     first ++n = 11 then * then n++ = 12 so 132 is answer.
     */
 
-    cout << ans << endl;
+
+
+    // cout << n++ * ++n << endl;
+    n = 10;
+    // ans = n++ * ++n;
+
+    cout << n++ * ++n << endl;
+
+
+    /*
+    first n++ = 11 then * then ++n = 1
+    */
 
     return 0;
 }
