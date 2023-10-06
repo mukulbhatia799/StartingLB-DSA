c3981007d622ce28cccf489c4a5e26d7a7776f24
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-06 23:32:38 +0530

    set kth bit and tell the deciamal value.

--- basics.cpp
+++ basics.cpp
@@ -2,15 +2,21 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
+    int n, k;
+    cin >> n >> k;
 
-    while(n != 0)
+    int pow2 = 1;
+    int ans = 0;
+    while(n)
     {
-        int mod = n % 10;
-        n /= 10;
-        cout << mod;
+        int checkbit = n & 1;
+        if(checkbit == 1 || k == 0) ans += pow2;
+        pow2 *= 2;
+        n = n >> 1;
+        k--;
     }
+
+    cout << ans << endl;
 
     return 0;
 }
