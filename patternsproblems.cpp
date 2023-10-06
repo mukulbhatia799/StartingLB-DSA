a4d0bf5ddc3dc711a8873c34a21d57a824839c97
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-06 22:00:32 +0530

    solved numeric hollow inverted half pyramid pattern problem

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -1,32 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+int main(){
     int n;
     cin >> n;
-    
-    // n should be even.
-    int stars = 1, spaces = n-1;
-    for(int i = 1; i <= n; i++)
+
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 1; j <= stars; j++)
-            cout << "* ";
-        for(int j = 1; j <= spaces; j++)
-            cout << "  ";
-        for(int j = 1; j <= stars; j++)
-            cout << "* ";
+        for(int j = 0; j < n-i; j++)
+        {
+            if(i == 0 || j == 0 || j == n-i-1)
+                cout << j+i+1 << " ";
+            else 
+                cout << "  ";
+        }
         cout << endl;
-        if(i < (n+1)/2)
-        {
-            stars++;
-            spaces-=2;
-        }
-        else if(i == (n+1)/2) continue;
-        else 
-        {
-            stars--;
-            spaces+=2;
-        }
     }
 
     return 0;
