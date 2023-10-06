fbfcd24e89d5ca80f4a13ec22727b7646b7c491e
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-06 22:37:07 +0530

    given number is odd or even using bitwise and operator.

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -5,27 +5,10 @@ int main(){
     int n;
     cin >> n;
 
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= n; j++)
-        {
-            if(i <= n/2)
-            {
-                if(j <= i || j >= n-i+1)
-                    cout << "* ";
-                else 
-                    cout << "  ";
-            }
-            else 
-            {
-                if(j <= n-i+1 || j >= i)
-                    cout << "* ";
-                else 
-                    cout << "  ";
-            }
-        }
-        cout << endl;
-    }
+    // given number is even or odd using bitwise operator.
+
+    if(n & 1) cout << "number is odd. " << endl;
+    else cout << "number is even." << endl;
 
     return 0;
 }
