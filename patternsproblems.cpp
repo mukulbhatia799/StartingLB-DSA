591841ebdbdf666748aeb9d1fdd7ed353a90430f
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-06 22:33:40 +0530

    solved butterfly pattern problem(done best)

--- patternsproblems.cpp
+++ patternsproblems.cpp
@@ -7,15 +7,22 @@ int main(){
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n-i; j++)
-            cout << "  ";
-        int j = 1, num = 1;
-        while(j <= 2*i-1)
+        for(int j = 1; j <= n; j++)
         {
-            cout << num << " ";
-            if(j < i) num++;
-            else num--;
-            j++;
+            if(i <= n/2)
+            {
+                if(j <= i || j >= n-i+1)
+                    cout << "* ";
+                else 
+                    cout << "  ";
+            }
+            else 
+            {
+                if(j <= n-i+1 || j >= i)
+                    cout << "* ";
+                else 
+                    cout << "  ";
+            }
         }
         cout << endl;
     }
