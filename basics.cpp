b8aabee1bf593736cb4c32dbd2e64d973090e812
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-07 22:41:59 +0530

    added size of linked list function

--- basics.cpp
+++ basics.cpp
@@ -1,21 +1,74 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-
-    vector<int> vec;
-    while(n)
+class Node
+{
+    int data;
+    Node* next;
+    Node* head;
+public:
+    Node()
     {
-        int mod = n % 10;
-        n /= 10;
-        vec.push_back(mod);
+        this->data = 0;
+        this->next = NULL;
+        this->head = NULL;
     }
-    cout << "all digits are: ";
-    for(int i = vec.size()-1; i >= 0; i--)
-        cout << vec[i] << " ";
-    cout << endl;
+    void sizeOfList()
+    {
+        Node* temp = head;
+        int count = 0;
+        while(temp)
+        {
+            count++;
+            temp = temp->next;
+        }
+        cout << "Current size of List is " << count << endl;
+    }
+    void insertAtHead()
+    {
+        Node* temp = new Node;
+        cout << "Enter value to insert at head: ";
+        cin >> temp->data;
+        temp->next = head;
+        head = temp;
+    }
+    void insertAtEnd()
+    {
+        Node* temp = new Node;
+        Node* temp2 = head;
+        cout << "Enter value to insert at end: ";
+        cin >> temp->data;
+        while(temp2->next != NULL)
+        {
+            temp2 = temp2->next;
+        }
+        temp2->next = temp;
+    }
+    void display()
+    {
+        Node* temp = head;
+        cout << "Displaying linked list: ";
+        while(temp)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main(){
+    Node obj;
+    obj.insertAtEnd();
+    obj.insertAtEnd();
+    obj.insertAtEnd();
+    obj.display();
+    obj.sizeOfList();
+    obj.insertAtEnd();
+    obj.insertAtHead();
+    obj.display();
+    obj.sizeOfList();
+    
 
     return 0;
 }
