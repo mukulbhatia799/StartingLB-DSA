f90f41dd3c154427612b5410d5eeeeea25adf98e
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-14 16:10:41 +0530

    right shift an array by k

--- assignmentQues.cpp
+++ assignmentQues.cpp
@@ -1,20 +1,31 @@
-// right shift an array by 1.
+// right shift an array by k.
 // input:  1 2 3 4 5 6
-// output: 6 1 2 3 4 5
+// k = 3
+// output: 4 5 6 1 2 3
 #include<iostream>
 using namespace std;
 
-void rightShiftAnArrayBy1(int arr[], int n)
+void rightShiftByKElements(int arr[], int n, int k)
 {
-    int left = 0, right = n-2;
-    // first swap the elments from 0th to n-1 elements.
+    k = k % n;      // this statment will be beneficial when the number of shift is greater than the number of elements because w know that after every n rounds the array will be as same as know. so, thatswhy we are taking mod of that k number.
+
+    // reverse the left array.
+    int left = 0, right = n - k - 1;
     while(left < right)
     {
         swap(arr[left], arr[right]);
         left++;
         right--;
     }
-    // now, reverse the array
+    // reverse the right array
+    left = n - k, right = n-1;
+    while(left < right)
+    {
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+    // reverse the whole array
     left = 0, right = n-1;
     while(left < right)
     {
@@ -25,14 +36,14 @@ void rightShiftAnArrayBy1(int arr[], int n)
 }
 
 int main(){
-    int n;
-    cin >> n;
+    int n, k;
+    cin >> n >> k;
 
     int arr[n];
     cout << "input array: ";
     for(int i = 0; i < n; i++) cin >> arr[i];
 
-    rightShiftAnArrayBy1(arr, n);
+    rightShiftByKElements(arr, n, k);
 
     cout << "After shifting: "; 
     for(int i = 0; i < n; i++)
