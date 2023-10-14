078a1fce436658cdfd49795c85be5d04a99779a3
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-14 14:20:05 +0530

    print all permutations of elements in an array

--- assignmentQues.cpp
+++ assignmentQues.cpp
@@ -1,28 +1,23 @@
-// find unique element in an array(where all elements have 1 occurence except 1 element. Find that element.).
-
 #include<iostream>
 using namespace std;
 
-int findUnique(int arr[], int n)
+void printAllPermutations(int arr[], int n)
 {
-    int XOR = 0;
     for(int i = 0; i < n; i++)
     {
-        XOR ^= arr[i];
+        for(int j = 0; j < n; j++)
+        {
+            cout << "{" << arr[i] << ", " << arr[j] << "}" << endl;
+        }
+        cout << endl;
     }
-    return XOR;
 }
 
 int main(){
-    int n;  // n should be odd as 2*(number of occ.) + 1 unique element.
-    cin >> n;
+    int arr[]={10, 20, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int arr[n];
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-    
-    int unique = findUnique(arr, n);
-    cout << "unique element is " << unique << endl;
+    printAllPermutations(arr, n);
 
     return 0;
 }
