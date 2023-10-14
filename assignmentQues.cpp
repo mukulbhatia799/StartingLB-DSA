92622a9c219fcad6392963b70914b10a9c78e43e
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-14 13:54:38 +0530

    Extreme print in an array

--- assignmentQues.cpp
+++ assignmentQues.cpp
@@ -1,14 +1,25 @@
-// reverse an array.
+// expreme print in an array.
+// eg: 10 20 30 40 50 60.
+// output:- 10 60 20 50 30 40.
 
 #include<iostream>
 using namespace std;
 
-void reverseAnArray(int arr[], int n)
+void extremePrint(int arr[], int n)
 {
-    for(int i = 0; i < n/2; i++)
+    int low = 0, high = n-1;
+    while(low <= high)
     {
-        swap(arr[i], arr[n-i-1]);
+        if(low == high) {
+            cout << arr[low] << endl;
+            return ;
+        }
+        cout << arr[low] << " ";
+        cout << arr[high] << " ";
+        low++;
+        high--;
     }
+    cout << endl;
 }
 
 int main(){
@@ -20,13 +31,10 @@ int main(){
     {
         cin >> arr[i];
     }
+    cout << endl;
 
-    reverseAnArray(arr, n);
-
-    for(int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }    
+    cout << "new array: " << endl;
+    extremePrint(arr, n); 
 
     return 0;
 }
