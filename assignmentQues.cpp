bd8a627e0976e72630dd2723dc2e7343f24778a9
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-15 16:33:07 +0530

    Solved Missing value in list ques. Range of elements is [0,N]. Find the missing value.

--- assignmentQues.cpp
+++ assignmentQues.cpp
@@ -1,36 +1,20 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<unordered_map>
+#include<bits/stdc++.h>
 using namespace std;
 
-pair<int, int> findsum(int n, int target)
-{
-    unordered_map<int, int> mpp;
-    int val;
-    for(int i = 0; i < n; i++)
-    {
-        cin >> val;
-        mpp[val] = i;
-    }
-    for(auto it = mpp.begin(); it != mpp.end(); it++)
-    {
-        int comp = target - (it->first);
-        auto found = mpp.find(comp);
-        if(found != mpp.end())
-        {
-            return {it->second, found->second};
-        }
-    }
-}
-
 int main(){
-    int n, target;
-    cin >> n >> target;
+    vector<int> vec{3, 0, 1, 2, 5};
+    vector<int> store(vec.size()+1, -1);
+    for(int i = 0; i < vec.size(); i++)
+    {
+        store[vec[i]] = 0;
+    }
 
-    pair<int, int> prr = findsum(n, target);
+    auto it = find(store.begin(), store.end(), -1);
 
-    cout << "value found at index {" << prr.first << ", " << prr.second << "}." << endl;
-    
+    if(it != store.end())
+    {
+        cout << "Missing value is: " << it - store.begin() << endl;
+    }
+
     return 0;
 }
