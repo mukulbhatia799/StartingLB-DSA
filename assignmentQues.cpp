98a7504b38385f23555f9013322ea984fadbbf99
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-16 11:20:24 +0530

    find pivot index ques solved using two pointers approach

--- assignmentQues.cpp
+++ assignmentQues.cpp
@@ -1,20 +1,41 @@
+// find pivot index:- find the index where the exact left and exact right sum is equal without including the pivot index.
+// If pivot index not found than return -1.
 #include<bits/stdc++.h>
 using namespace std;
 
+int findPivotIndex(int arr[], int n)
+{
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+
+    int leftsum = 0, rightsum = sum;
+    for(int i = 0; i < n; i++)
+    {
+        if(i > 0) {
+            leftsum += arr[i-1];
+        }
+        rightsum -= arr[i];
+        if(leftsum == rightsum) return i;
+    }
+    return -1;
+}
+
 int main(){
-    vector<int> vec{3, 0, 1, 2, 5};
-    vector<int> store(vec.size()+1, -1);
-    for(int i = 0; i < vec.size(); i++)
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
     {
-        store[vec[i]] = 0;
+        cin >> arr[i];
     }
 
-    auto it = find(store.begin(), store.end(), -1);
+    int ans = findPivotIndex(arr, n);
 
-    if(it != store.end())
-    {
-        cout << "Missing value is: " << it - store.begin() << endl;
-    }
+    cout << ans << endl;
 
     return 0;
 }
