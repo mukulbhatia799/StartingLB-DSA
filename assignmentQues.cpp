5dae22ad1c3e297ca95c190af3fd6eed94529675
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-17 18:16:52 +0530

    Solved...Find missing elements which are not present in array. Range of elements is [1,N].

--- assignmentQues.cpp
+++ assignmentQues.cpp
@@ -1,43 +1,79 @@
-// remove duplicates from sorted array using two pointers approach.
+// Missing elements from an array with duplicates. Range of elements is [0, N].
 /*
+Arroach 1: 
+We can name this approach as index as elements.
+Range of elements is   [1, N]    thaswhy we can use indexes as elements.
+As the index starts from 0 so we have to make element -ve at 
+arr[element-1]. We will make the element present in elements 
+index as -ve because we are traversing the array so we also need 
+the values which will come in the coming iterations. 
+In each iteration, we will take the abs of that value because 
+the element might be changed to -ve in previous iterations.
 
-Method->(two pointers appraoch): First pointer will point till the unique elements 
-and second pointer will traverse to the whole array in each itertion and check whether 
-the second pointer element is equal to first pointer element or not. If not then first++ then swap and then second++;
-Time Complexity: O(n)       Space Complexity: O(1).
+Approach 2:
+Use elements as index and make the elements index element as -ve. In next loop, find those values which are positive. Positive elements means those elements indexes are not present in array.
 
-
+Time complexity: O(n)   Space complexity: O(n)
 */
-#include<bits/stdc++.h>
+
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int removeDuplicates(int arr[], int n)
+
+// Approach 1
+vector<int> MissingValueUsingElementsAsIndex(int arr[], int n)
 {
-    int ptr1 = 0, ptr2 = 1;
-    while(ptr2 < n)
+    vector<int> ans;
+    for(int i = 0; i < n; i++)
     {
-        if(arr[ptr2] != arr[ptr1])  // here 
+        int index = arr[i]-1;
+        if(arr[index] != arr[i]) 
         {
-            // ptr1++;
-            // swap(arr[ptr1], arr[ptr2]);
-            // ptr2++;
-            swap(arr[++ptr1], arr[ptr2++]); // prev. 3 lines of code in just one line. ptr1 should be pre increment first, and ptr2 should be post increment means first the ptr2 value is store as index in arr and after swapping then the ptr2 value is updated to +1.
+            swap(arr[index], arr[i]);
         }
-        else ptr2++;
+        else ++i;
     }
+    for(int i = 0; i < n; i++) 
+    {
+        if(arr[i]-1 != i) ans.push_back(i+1);
+    }
+    
+    return ans;
+}
+
+
+// Approach 2: 
+vector<int> MissingValueByPlacingElementIndexAsNegative(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        int index = abs(arr[i]);    // abs is required the element at ith index might be changed to index during previous iterations.
+        if(arr[index-1] > 0) arr[index-1] *= -1;
+    }
+
+    vector<int> ans;
+    for(int i = 0; i < n; i++) 
+    {
+        if(arr[i] > 0) ans.push_back(i+1);
+    }
+
+    return ans;
 }
 
 int main(){
-    int n;
-    cin >> n;
+    int n = 5;
+    // cin >> n;
 
-    int arr[n];
-    for(int i = 0; i < n; i++) cin >> arr[i];
+    int arr[n]={1, 3, 3, 3, 3};
+    // for(int i = 0; i < n; i++) cin >> arr[i];
 
-    int k = removeDuplicates(arr, n);       // return the index till all elements are unique.
+    vector<int> ans = MissingValueByPlacingElementIndexAsNegative(arr, n);
+    // vector<int> ans = MissingValueUsingElementsAsIndex(arr, n);
 
-    cout << "After removing duplicates: ";
-    for(int i = 0; i < n; i++) cout << arr[i] << ' ';
+    cout << "Missing elements: ";
+    for(auto it: ans) cout << it << " ";
+    cout << endl;
 
     return 0;
 }
