18fa2a6717550f3b056ae3450f00d101cd3471df
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-17 18:34:27 +0530

    gfg First Repeating Element ques solved using map.

--- assignmentQues.cpp
+++ assignmentQues.cpp
@@ -1,79 +1,33 @@
-// Missing elements from an array with duplicates. Range of elements is [0, N].
-/*
-Arroach 1: 
-We can name this approach as index as elements.
-Range of elements is   [1, N]    thaswhy we can use indexes as elements.
-As the index starts from 0 so we have to make element -ve at 
-arr[element-1]. We will make the element present in elements 
-index as -ve because we are traversing the array so we also need 
-the values which will come in the coming iterations. 
-In each iteration, we will take the abs of that value because 
-the element might be changed to -ve in previous iterations.
+// ques link: https://practice.geeksforgeeks.org/problems/first-repeating-element4018/1
 
-Approach 2:
-Use elements as index and make the elements index element as -ve. In next loop, find those values which are positive. Positive elements means those elements indexes are not present in array.
-
-Time complexity: O(n)   Space complexity: O(n)
-*/
-
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-
-// Approach 1
-vector<int> MissingValueUsingElementsAsIndex(int arr[], int n)
+int firstRepeatingElement(int arr[], int n)
 {
-    vector<int> ans;
+    unordered_map<int, int> mpp;
+
     for(int i = 0; i < n; i++)
     {
-        int index = arr[i]-1;
-        if(arr[index] != arr[i]) 
-        {
-            swap(arr[index], arr[i]);
-        }
-        else ++i;
+        mpp[arr[i]]++;
     }
-    for(int i = 0; i < n; i++) 
-    {
-        if(arr[i]-1 != i) ans.push_back(i+1);
-    }
-    
-    return ans;
-}
 
-
-// Approach 2: 
-vector<int> MissingValueByPlacingElementIndexAsNegative(int arr[], int n)
-{
     for(int i = 0; i < n; i++)
     {
-        int index = abs(arr[i]);    // abs is required the element at ith index might be changed to index during previous iterations.
-        if(arr[index-1] > 0) arr[index-1] *= -1;
+        if(mpp[arr[i]] > 1) return i+1;
     }
 
-    vector<int> ans;
-    for(int i = 0; i < n; i++) 
-    {
-        if(arr[i] > 0) ans.push_back(i+1);
-    }
-
-    return ans;
+    return -1;  // If none of the values is repeating.
 }
 
 int main(){
-    int n = 5;
-    // cin >> n;
+    int n = 7;
 
-    int arr[n]={1, 3, 3, 3, 3};
-    // for(int i = 0; i < n; i++) cin >> arr[i];
+    int arr[n] = {1, 5, 3, 4, 3, 5, 6};
 
-    vector<int> ans = MissingValueByPlacingElementIndexAsNegative(arr, n);
-    // vector<int> ans = MissingValueUsingElementsAsIndex(arr, n);
+    int index1based = firstRepeatingElement(arr, n);
 
-    cout << "Missing elements: ";
-    for(auto it: ans) cout << it << " ";
-    cout << endl;
+    cout << "Index of the first repeating element: " << index1based << endl;
 
     return 0;
 }
