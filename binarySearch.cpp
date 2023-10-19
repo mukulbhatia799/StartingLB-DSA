4ca0e72d79dccc1631b19dec1eb41215774a2453
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-19 18:01:40 +0530

    find total occurence of a number in sorted array. As sorted array, so using binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,16 +1,21 @@
-// find last occurence of a number in sorted array.
+// find total occurence of a number in sorted array.
 
 // As sorted array is given, so we can use binary search here.
+
+/*
+    To find total occurence, if we Know the first and last occ. then we can subt.
+    the indexes of first and last occ. to know the total occ. of K.
+*/
 
 #include<iostream>
 using namespace std;
 
-int findLastOccOfK(int arr[], int n, int k)
+int findLastOccOfK(int *arr, int n, int k)
 {
     int low = 0, high = n-1, ans = -1;
     while(low <= high)
     {
-        int mid = low + (high-low)/2;
+        int mid = low + (high-low)/2; 
         if(arr[mid] == k)
         {
             low = mid+1;
@@ -22,15 +27,43 @@ int findLastOccOfK(int arr[], int n, int k)
     return ans;
 }
 
+int findFirstOccOfK(int *arr, int n, int k)
+{
+    int low = 0, high = n-1, ans = -1;
+    while(low <= high)
+    {
+        int mid = low + (high - low)/2;
+        if(arr[mid] == k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else if(arr[mid] > k) high = mid - 1;
+        else low = mid + 1;
+    }
+    return ans;
+}
+
+int findTotalOccOfK(int *arr, int n, int k)
+{
+    int firstIndex = findFirstOccOfK(arr, n, k);
+    int lastIndex = findLastOccOfK(arr, n, k);
+
+    if(firstIndex == -1) return -1;
+    
+    return lastIndex-firstIndex+1;
+}
+
 int main(){
     int n, k;
     cin >> n >> k;
 
     int arr[n];
     for(int i = 0;i < n; i++) cin >> arr[i];
-    int indexOfK = findLastOccOfK(arr, n, k);
-    if(indexOfK == -1) cout << "k value not found." << endl;
-    else cout << "Last occ of k found at index " << indexOfK << endl;
+    int indexOfK = findTotalOccOfK(arr, n, k);
+    cout << "Total occ. of k variable is ";
+    if(indexOfK == -1) cout << 0 << endl;
+    else cout << indexOfK << endl;
 
     return 0;
 }
