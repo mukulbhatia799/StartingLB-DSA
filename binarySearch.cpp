448fdfa3538491ed5cdff32cb5773ac294a3ed6c
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-19 17:46:06 +0530

    find last occurence of a number in sorted array. As sorted array, so using binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,11 +1,11 @@
-// find first occurence of a number in sorted array.
+// find last occurence of a number in sorted array.
 
 // As sorted array is given, so we can use binary search here.
 
 #include<iostream>
 using namespace std;
 
-int findFirstOccOfK(int arr[], int n, int k)
+int findLastOccOfK(int arr[], int n, int k)
 {
     int low = 0, high = n-1, ans = -1;
     while(low <= high)
@@ -13,7 +13,7 @@ int findFirstOccOfK(int arr[], int n, int k)
         int mid = low + (high-low)/2;
         if(arr[mid] == k)
         {
-            high = mid-1;
+            low = mid+1;
             ans = mid;
         }
         else if(arr[mid] > k) high = mid-1;
@@ -21,7 +21,6 @@ int findFirstOccOfK(int arr[], int n, int k)
     }
     return ans;
 }
-// 5 5 5 5 5 5 5 8
 
 int main(){
     int n, k;
@@ -29,9 +28,9 @@ int main(){
 
     int arr[n];
     for(int i = 0;i < n; i++) cin >> arr[i];
-    int indexOfK = findFirstOccOfK(arr, n, k);
+    int indexOfK = findLastOccOfK(arr, n, k);
     if(indexOfK == -1) cout << "k value not found." << endl;
-    else cout << "First occ of k found at index " << indexOfK << endl;
+    else cout << "Last occ of k found at index " << indexOfK << endl;
 
     return 0;
 }
