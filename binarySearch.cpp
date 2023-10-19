bfd7b682992697c66d102d29bd5830569c4e66d3
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-19 18:41:35 +0530

    find missing element in a sorted array.

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,69 +1,54 @@
-// find total occurence of a number in sorted array.
-
-// As sorted array is given, so we can use binary search here.
-
-/*
-    To find total occurence, if we Know the first and last occ. then we can subt.
-    the indexes of first and last occ. to know the total occ. of K.
-*/
+// find missing element in a sorted array.
+// Elements range is [1, N].
 
 #include<iostream>
+#include<climits>
+#include<math.h>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<iomanip>
+
 using namespace std;
 
-int findLastOccOfK(int *arr, int n, int k)
-{
-    int low = 0, high = n-1, ans = -1;
-    while(low <= high)
-    {
-        int mid = low + (high-low)/2; 
-        if(arr[mid] == k)
-        {
-            low = mid+1;
-            ans = mid;
-        }
-        else if(arr[mid] > k) high = mid-1;
-        else low = mid+1;
-    }
-    return ans;
-}
+/*
 
-int findFirstOccOfK(int *arr, int n, int k)
+   eg: 1 2 3 4 6 7 8 9
+index: 0 1 2 3 4 5 6 7
+As we can see, each value should be at element-1 index. So we just have to check 
+from where the element and its index difference are not 1.
+
+*/
+int findMissingElement(int* arr, int n)
 {
     int low = 0, high = n-1, ans = -1;
     while(low <= high)
     {
         int mid = low + (high - low)/2;
-        if(arr[mid] == k)
+        if(arr[mid]-mid != 1)
         {
-            ans = mid;
-            high = mid - 1;
+            ans = mid+1;
+            high = mid-1;
         }
-        else if(arr[mid] > k) high = mid - 1;
-        else low = mid + 1;
+        else {
+            low = mid+1;
+        }
     }
     return ans;
 }
 
-int findTotalOccOfK(int *arr, int n, int k)
-{
-    int firstIndex = findFirstOccOfK(arr, n, k);
-    int lastIndex = findLastOccOfK(arr, n, k);
-
-    if(firstIndex == -1) return -1;
-    
-    return lastIndex-firstIndex+1;
-}
-
 int main(){
-    int n, k;
-    cin >> n >> k;
+    int n = 8;
+    int arr[n]={1, 2, 3, 4, 6, 7, 8, 9};    // Here missing element is 5.
 
-    int arr[n];
-    for(int i = 0;i < n; i++) cin >> arr[i];
-    int indexOfK = findTotalOccOfK(arr, n, k);
-    cout << "Total occ. of k variable is ";
-    if(indexOfK == -1) cout << 0 << endl;
-    else cout << indexOfK << endl;
+    int missingele = findMissingElement(arr, n);
 
+    if(missingele == -1) cout << "Missing element not found" << endl;
+    else cout << "Missing element is " << missingele << endl;
+    
     return 0;
 }
