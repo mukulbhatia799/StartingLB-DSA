4683484021815e1f81d4e893ee76dc664613f597
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-19 21:41:01 +0530

    Print diagonals of 2D matrix ques solved(not binary search ques.

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,22 +1,3 @@
-// Find Peak Index in a Mountain Array.
-// leetcode problem
-
-/*
-
-    This ques can be done using linear search with Time Complexity O(n).
-    But using binary search we can solve this ques in O(logn) time complexity.
-
-    ***The key point in this ques is that first the array will be in increasing order
-    and after the peak index it starts decreasing.***
-
-    So, the intution is when the arr[mid] element is greater than its next element, 
-    move high = mid;
-
-
-    **extra**   When low and high diff. is 1 then mid = (low+high)/2 = low.
-
-*/
-
 #include<iostream>
 #include<climits>
 #include<math.h>
@@ -31,35 +12,38 @@
 
 using namespace std;
 
-
-int findPeakElementIndex(int* arr, int n)
+void printDiagonals(vector<vector<int>> arr, int size)
 {
-    int low = 0, high = n-1;
-    int mid;
-    while(low < high)           // We will not take low <= high because last element will be our peak element.
+    int j = 0;
+    cout << "1st diagonal: ";
+    for(int i = 0; i < size; i++)
     {
-        mid = low + (high - low) / 2;   // same as mid = (low + high) / 2;
-        cout << "low: " << low << " high: " << high << " mid: " << mid << endl;
-        if(arr[mid] < arr[mid+1])
-        {
-            low = mid + 1;
-        }
-        else {
-            high = mid;
-        }
+        cout << arr[i][i] << " ";
     }
-    return low;
+    cout << endl;
+    cout << "2nd diagonal: ";
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i][size-i-1] << " ";
+    }
 }
+
 
 int main(){
     int n;
+    cout << "Enter n*n matrix size: ";
     cin >> n;
-    int arr[n];
 
-    for(int i = 0; i < n; i++) cin >> arr[i];
+    vector<vector<int>> vec(n, vector<int>(n));
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> vec[i][j];
+        }
+    }
 
-    int index = findPeakElementIndex(arr, n);
-    cout << "peak element index: " << arr[index] << endl;
+    printDiagonals(vec,n);
     
     return 0;
 }
