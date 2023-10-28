7fc679df21d470c8de8f3aa22165d6eb3f73a5f8
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-28 19:33:12 +0530

    nth fibonacci number iterative approach in O(n) time complexity.

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,5 @@
-// fibonacci series using recursive approach. 
-// Time complexity is exponential means O(2^n).
-// space complexity is O(n). As maximum depth is o(n).
+// iterative approach for fibonacci series.
+
 #include<iostream>
 #include<climits>
 #include<math.h>
@@ -13,21 +12,29 @@
 #include<unordered_set>
 #include<iomanip>
 
+int iterative_app_fibSeries(int n)
+{
+    int first = 0, second = 1, nthNumber = 0;
+    if(n == 1) return 0;
+    else if(n == 2) return 1;
+    for(int i = 3; i <= n; i++)
+    {
+        nthNumber = first + second;
+        first = second;
+        second = nthNumber;
+    }
+    return nthNumber;
+}
+
 using namespace std;
 
-int fib_series_using_recursion(int n)
-{
-    if(n == 1) return 0;
-    if(n == 2) return 1;
-    // if(n <= 2) return n-1;
-    return fib_series_using_recursion(n-1) + fib_series_using_recursion(n-2);
-}
 
 int main(){
     int n;
     cin >> n;
 
-    cout << "nth fib is " << fib_series_using_recursion(n) << endl;
+    cout << "nth fib. no is: " << iterative_app_fibSeries(n) << endl;
+
     
     return 0;
 }
