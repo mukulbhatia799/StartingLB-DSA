90130223fe45251b6ca01f348b62350ffb4736ca
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-28 19:26:01 +0530

    fibonacci series using recursive approach. T.C exponential means O(2^n) and S.C is O(n) as max. depth is O(n).

--- recursion.cpp
+++ recursion.cpp
@@ -1,28 +1,33 @@
-// leetcode: 70 climbing stairs.
-// Using recursion, time complexity is O(2^n) but using dp, can be done using O(n).
+// fibonacci series using recursive approach. 
+// Time complexity is exponential means O(2^n).
+// space complexity is O(n). As maximum depth is o(n).
 #include<iostream>
-#include<vector>
+#include<climits>
+#include<math.h>
+#include<cmath>
 #include<algorithm>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<iomanip>
 
 using namespace std;
 
-int noofsteps(int steps){
-    static int count = 0;
-    if(steps <= 0)
-    {
-        if(steps == 0) count++;
-        return 0;
-    }
-    noofsteps(steps-1);
-    noofsteps(steps-2);
-    return count;
+int fib_series_using_recursion(int n)
+{
+    if(n == 1) return 0;
+    if(n == 2) return 1;
+    // if(n <= 2) return n-1;
+    return fib_series_using_recursion(n-1) + fib_series_using_recursion(n-2);
 }
 
 int main(){
     int n;
     cin >> n;
 
-    cout << "No. of ways to reach n: " << noofsteps(n) << endl;
-
+    cout << "nth fib is " << fib_series_using_recursion(n) << endl;
+    
     return 0;
 }
