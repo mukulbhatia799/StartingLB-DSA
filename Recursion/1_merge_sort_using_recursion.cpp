60c61907ec206138545bb87af6c127153021b94f
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-11-10 18:47:38 +0530

    remove debugging lines and added comments for better understanding

--- Recursion/1_merge_sort_using_recursion.cpp
+++ Recursion/1_merge_sort_using_recursion.cpp
@@ -1,43 +1,28 @@
 #include<iostream>
 using namespace std;
 
-void Merge(int* arr, int l, int mid, int r, int actualsize)
-{
+// recursive tree for better understanding.
+
+void Merge(int* arr, int l, int mid, int r, int actualsize) {
     int leftsize = mid-l+1;
     int rightsize = r-mid;
-
-    // cout << "leftsize: " << leftsize << endl;   //
-    // cout << "rightsize: " << rightsize << endl; //
-    // cout << "l: " << l << endl; //
-    // cout << "mid: " << mid << endl; //
-    // cout << "r: " << r << endl; //
 
     int *leftarr = new int[leftsize];
     int *rightarr = new int[rightsize];
     int traverse = 0;
-    // cout << "leftarr: ";    //
-    while(traverse < leftsize)
-    {
+    while(traverse < leftsize) {                    // storing left side array in leftarr
         leftarr[traverse] = arr[l+traverse];
-        // cout << leftarr[traverse] << " "; //
         traverse++;
     }
-    // cout << endl;   //
     traverse = 0;
-    // cout << "rightarr: ";    //
-    while(traverse < rightsize)
-    {
+    while(traverse < rightsize) {                   // storing right side array in rightarr
         rightarr[traverse] = arr[mid+1+traverse];
-        // cout << rightarr[traverse] << " "; //
         traverse++;
     }
-    // cout << endl;   //
     int leftptr = 0, rightptr = 0;
     traverse = l;
-    while(leftptr < leftsize && rightptr < rightsize)
-    {
-        if(leftarr[leftptr] <= rightarr[rightptr])
-        {
+    while(leftptr < leftsize && rightptr < rightsize) {     // comparing each value of leftarr and rigth arr, and storing the lesser value in actual arr first until one of the array is not finished.
+        if(leftarr[leftptr] <= rightarr[rightptr]) {
             arr[traverse] = leftarr[leftptr];
             traverse++, leftptr++;
         }
@@ -46,12 +31,10 @@ void Merge(int* arr, int l, int mid, int r, int actualsize)
             traverse++, rightptr++;
         }
     }
-    while(leftptr < leftsize)
-    {
+    while(leftptr < leftsize) {                 // if some elements left in leftarr, then place it in actual array, as they are already sorted so no need to worry:)
         arr[traverse++] = leftarr[leftptr++];
     }
-    while(rightptr < rightsize)
-    {
+    while(rightptr < rightsize) {               // if some elements left in rightarr, then place it in actual array, as they are already sorted so no need to worry:)
         arr[traverse++] = rightarr[rightptr++];    
     }
 }
@@ -61,12 +44,9 @@ void mergeSort(int* arr, int l, int r, int actualsize)
     if(l >= r) return ;
 
     int mid = l + (r-l)/2;
-    mergeSort(arr, l, mid, actualsize);
-    mergeSort(arr, mid+1, r, actualsize);
-    Merge(arr, l, mid, r, actualsize);
-    // cout << "arr after iteration: ";    //
-    // for(int i = 0; i < actualsize; i++) cout << arr[i] << " ";  //
-    // cout << endl << endl;   //
+    mergeSort(arr, l, mid, actualsize);         // left side array recursive function
+    mergeSort(arr, mid+1, r, actualsize);       // right side array recursive function
+    Merge(arr, l, mid, r, actualsize);          // As, the last recursive iteration will be when we have both single element in left side and right side array, so we just want to compare and place in actual array. And continue till the actual arr is not sorted.
 }
 
 int main(){
@@ -81,7 +61,6 @@ int main(){
     mergeSort(arr, 0, size-1, size);
     cout << "After" << endl;
     for(int i = 0; i < size; i++) cout << arr[i] << " ";
-    cout << endl;
 
     return 0;
 }
