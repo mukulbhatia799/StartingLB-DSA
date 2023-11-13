59c2592dff32d9b2df78142565a5846083d7fec3
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-11-13 16:15:20 +0530

    quick sort. let last index as pivot

---
+++ Recursion/4_QuickSort.cpp
@@ -0,0 +1,54 @@
+// Sort array using Quick Sort
+
+/*
+
+Different approach: -
+1. Let i be at -1 index and j be at 0th index. When jth value is less than pivotIndex, then ++i and swap ith and jth value so that at every iteration of while loop, we place all smaller value than pivotIndex at left place. And when while loop ends, ++i and place pivot element at ith index.
+2. After, do this recursively so that both left side and right side of pivot is placed at there correct place. 
+
+*/
+
+#include<iostream>
+using namespace std;
+
+void placePivotAtRightPlace(int* arr, int start, int pivotIndex)
+{
+    if(start >= pivotIndex) return ;
+    int i = start-1;
+    int j = start;
+
+    while(j < pivotIndex)
+    {
+        if(arr[j] < arr[pivotIndex]) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+        j++;
+    }
+    i++;
+    swap(arr[i], arr[pivotIndex]);
+
+    placePivotAtRightPlace(arr, start, i-1);
+    placePivotAtRightPlace(arr, i+1, pivotIndex);
+
+}
+
+void sortUsingQuickSort(int* arr, int size)
+{
+    placePivotAtRightPlace(arr, 0, size-1);
+}
+
+int main(){
+    int arr[] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int size = sizeof(arr) / sizeof(int);
+    cout << "Before ";
+    for(int i = 0; i < size; i++) cout << arr[i] << " ";
+    cout << endl;
+    sortUsingQuickSort(arr, size);
+
+    cout << "After ";
+    for(int i = 0; i < size; i++) cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
